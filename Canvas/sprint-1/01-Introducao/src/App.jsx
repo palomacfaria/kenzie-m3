7e8c208888d01c2581cc5ef55219019a52c5c973
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-11-22 17:16:34 -0300

    Criando e exportando componentes

--- Canvas/sprint-1/01-Introducao/src/App.jsx
+++ Canvas/sprint-1/01-Introducao/src/App.jsx
@@ -1,24 +1,11 @@
+import { Component } from "./components/Component";
+
 function App() {
-  //Javascript
-const person = {
-  nome: "Paloma",
-  modulo: "M3",
-  idade: "23",
-}
-
-const alertName = () => {
-  alert(person.nome);
-}
-
-  //JSX
+  
   return (
-    <>
-    <li>
-      <h3>{person.nome} - {person.modulo}</h3>
-      <p>Tenho {person.idade} anos.</p>
-      <button onClick={alertName}>Quem sou eu?</button>
-    </li>
-    </>
+    <div className="App">
+      <Component/>
+    </div>
   )
 }
 
