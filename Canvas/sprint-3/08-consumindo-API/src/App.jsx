7b85fb191fe11d3d8621d97866c67385cb09c7d6
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-12-11 16:47:31 -0300

    Finalização do consumo da API ViaCep

--- Canvas/sprint-3/08-consumindo-API/src/App.jsx
+++ Canvas/sprint-3/08-consumindo-API/src/App.jsx
@@ -1,21 +1,63 @@
 import { useEffect, useState } from "react";
 
 function App() {
-
   const [adress, setAdress] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState("");
 
   useEffect(() => {
-    const loadData = async() => {
-      const response = await fetch("⁠https://viacep.com.br/ws/01001000/json/");
+    const loadData = async () => {
+      setLoading(true);
+      const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      const json = await response.json();
+      setAdress(json);
+      setLoading(false);
+    };
+    if (cep !== "") {
+      loadData();
     }
   }, []);
 
+  const submit = (e) => {
+    e.preventDefault();
+    setCep(input);
+  };
+
+  const cleanAdress = () => {
+    setAdress(null);
+    setCep("");
+    setInput("");
+  };
+
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      {loading ? (
+        <p>Carregando...</p>
+      ) : (
+        <>
+          {adress ? (
+            <div>
+              <h1>Meu endereço:</h1>
+              <p>
+                {adress.cep} - {adress.logradouro} - {adress.bairro} -{" "}
+                {adress.localidade} - {adress.uf}
+              </p>
+              <button onClick={cleanAdress}>Limpar endereço</button>
+            </div>
+          ) : (
+            <div>
+              <h1>Forneça seu cep para proseguir....</h1>
+              <form onSubmit={submit}>
+                <input type="text"  value={input} onChange={(e) => setInput(e.target.value)} />
+                <button type="submit">Buscar endereço</button>
+              </form>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
